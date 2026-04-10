38509ec57bf7ed829a64a442339ab22dd0bc950f
Mat77100 <matteo.porrera@icloud.com>
2026-04-10 14:47:21 +0100

    Update SortingStuff.py

--- SortingStuff.py
+++ SortingStuff.py
@@ -1,11 +1,17 @@
-#sortin n' stuff
+
 import random as rng
 import tkinter as tk
+import time
+import threading
+global A
+A = []
+for i in range(1,101):
+    A.append(i)
 
 
 #randomiser
 def randomiser(A):
-    for i in range(len(A)):
+    for i in range(0,len(A)-1):
         r1 = rng.randint(0,len(A)-1)
         r2 = rng.randint(0,len(A)-1)
         A[r1], A[r2] = A[r2], A[r1]
@@ -44,87 +50,159 @@ def QuickSort(A):
 
 #print("sorted array: ",QuickSort(A))
 
-#selection sort
-def SelectionSort(A):
+def SelectionSort():
+    global sorted,A,speed
+    if sorted:
+        randbtn.config(state="normal")
+        sortbtn.config(state="normal")
+        sorted=True
     for i in range(len(A)):
-        min_idx = i
+        minpos = i #assume each new pos is the smallest
         for j in range(i+1, len(A)):
-            if A[min_idx] > A[j]:
-                min_idx = j
-        if min_idx!=i:
-            A[i], A[min_idx] = A[min_idx],A[i]
-    return A
+            if A[j] < A[minpos]:
+                minpos = j #smaller value found (inside loop)
+        if minpos != i: #if they dont match (outside loop)
+            A[i],A[minpos] = A[minpos],A[i]
+        time.sleep(speed/1000)
+    sorted = True
+    randbtn.config(state="normal")
+    sortbtn.config(state="normal")
 
-def BubbleSort(A):
+def BubbleSort():
+    global A,sorted,speed
+    if sorted:
+        randbtn.config(state="normal")
+        sortbtn.config(state="normal")
+        sorted=True
     for j in range(len(A)-1):
         for i in range(len(A)-j-1):
             if A[i] > A[i+1]:
                 A[i],A[i+1] = A[i+1],A[i]
-    return A
+                time.sleep(speed/1000)
+    sorted = True
+    randbtn.config(state="normal")
+    sortbtn.config(state="normal")
 
-def printQS():
-    print(QuickSort(A))
-    randomiser(A)
-def printSS():
-    print(SelectionSort(A))
-    randomiser(A)
-def printBS():
-    print(BubbleSort(A))
-    randomiser(A)
-def printArr():
-    print(A)
 
-def nextwin():
-    global A
-    A = makeRandomisedArr(S.get())
-    rootWin.withdraw()
-    
-    win = tk.Toplevel(rootWin)
+def BubbleStep():
+    global A,i,j,sorted,speed
+    if j < (len(A)-1) and not sorted:
+        if i <(len(A)-j-1):
+            if A[i] > A[i+1]:
+                A[i],A[i+1] = A[i+1],A[i]
+            i += 1
+            main.after(speed,BubbleStep)
+        else:
+            i = 0
+            j += 1
+            main.after(speed,BubbleStep)
+    else:
+        randbtn.config(state="normal")
+        sortbtn.config(state="normal")
+        sorted = True
 
-    l2 = tk.Label(win,text = "Pick a sorting algorythm:")
-    pb = tk.Button(win, text = "Print randomised array", width = 20, command = printArr)
-    b1 = tk.Button(win, text = "Quick Sort", width = 30, command=printQS)
-    b2 = tk.Button(win, text = "Selection Sort", width =30, command=printSS)
-    b3 = tk.Button(win, text = "Bubble Sort", width = 30, command=printBS)
 
-    pb.pack()
-    l2.pack()
-    b1.pack()
-    b2.pack()
-    b3.pack()
-
-rootWin = tk.Tk()
-
-S = tk.Scale(rootWin, from_=10, to=10000, orient="horizontal",resolution=10, length = 300, width = 20)
-l1 = tk.Label(rootWin, text = "Amount of element in the unsorted array:")
-NB = tk.Button(rootWin, text = "Next", width = 10, command=nextwin)
-S.pack()
-l1.pack()
-NB.pack()
-
-rootWin.mainloop()
+def randFun():
+    global sorted,A
+    randbtn.config(state="disabled")
+    sortbtn.config(state="disabled")
+    global A,lineList,Canvas
+    A = randomiser(A)
+    sorted = False
+    randbtn.config(state="normal")
+    sortbtn.config(state="normal")
 
 
 
-'''
-print("please select a soring method:")
-print("1 -- Quick Sort")
-print("2 -- Selection Sort")
-print("3 -- Bubble Sort")
-while True:
-    try:
-        action = int(input("input: "))
-        break
-    except:
-        print("Invalid input")
+def StartSort():
+    global sorted,is_running
+    if sorted:
+        return
+    randbtn.config(state="disabled")
+    sortbtn.config(state="disabled")
+    global i, j,min_idx
+    i = 0
+    j = 0
+    min_idx = 0
+    SortThread = threading.Thread(target=SelectionSort)
+    SortThread.start()
+    is_running = True
 
-if action == 1:
-    print(QuickSort(A))
-elif action == 2:
-    print(SelectionSort(A))
-elif action == 3:
-    print(BubbleSort(A))
+def UpdateLineslive():
+    global sorted,lineList,speed,is_running
+    while True:
+        try:
+            if sorted and is_running:
+                if SortThread.is_alive():
+                    SortThread.join()
+                    is_running = False
+        except:
+            pass
+        while not sorted:
+            for i in range(len(lineList)):
+                x1,y1,x2,y2 = Canvas.coords(lineList[i])
+                Canvas.coords(lineList[i],x1,y1,x2,A[i]*5)  
+                time.sleep(0.0001)
 
-print(A)
 
-'''
+
+sorted = True
+def SlowSpeed():
+    global speed
+    speed = 50
+    slowbtn.config(bg="grey")
+    mediumbtn.config(bg="white")
+    fastbtn.config(bg="white")
+    zoombtn.config(bg="white")
+def MediumSpeed():
+    global speed
+    speed = 30
+    slowbtn.config(bg="white")
+    mediumbtn.config(bg="grey")
+    fastbtn.config(bg="white")
+    zoombtn.config(bg="white")
+def FastSpeed():
+    global speed
+    speed = 10
+    slowbtn.config(bg="white")
+    mediumbtn.config(bg="white")
+    fastbtn.config(bg="grey")
+    zoombtn.config(bg="white")
+def ZoomSpeed():
+    global speed
+    speed = 2
+    slowbtn.config(bg="white")
+    mediumbtn.config(bg="white")
+    fastbtn.config(bg="white")
+    zoombtn.config(bg="grey")
+
+speed = 10
+is_running = False
+updatelinesThread = threading.Thread(target=UpdateLineslive,daemon=True)
+main = tk.Tk()
+main.geometry("1500x700")
+tk.Button(main,text="QUIT",command=main.destroy).pack()
+Canvas = tk.Canvas(main,width=1250,height=600)
+randbtn = tk.Button(main,text="Randomise!",command=randFun)
+sortbtn = tk.Button(main,text="Sort!",command=StartSort)
+slowbtn = tk.Button(main,text="Slow",command=SlowSpeed,anchor="e",)
+mediumbtn = tk.Button(main,text="Medium",command=MediumSpeed,anchor="e",bg="grey")
+fastbtn = tk.Button(main,text="Fast",command=FastSpeed,anchor="e")
+zoombtn = tk.Button(main,text="ZOOM!",command=ZoomSpeed,anchor="e")
+slowbtn.pack(side="right")
+mediumbtn.pack(side="right")
+fastbtn.pack(side="right")
+zoombtn.pack(side="right")
+tk.Label(main,text="Speed:",anchor="e").pack(side="right")
+randbtn.pack()
+sortbtn.pack()
+Canvas.pack()
+distance = 19
+lineList = []
+for i in range(0,100):
+    line = Canvas.create_line(i+distance,501,i+distance,A[i]*5,fill="blue",width=10)
+    lineList.append(line)
+    distance += 10
+updatelinesThread.start()
+main.mainloop()
+
