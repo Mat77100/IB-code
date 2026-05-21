6a82365298ac2e8d16b4da556189f9d8ca0a2115
Mat77100 <matteo.porrera@icloud.com>
2026-05-21 13:00:36 +0100

    Add files via upload

--- tkinter game.py
+++ tkinter game.py
@@ -3,21 +3,62 @@ import tkinter as tk
 import threading
 from time import *
 
-def StartPlayerMoveUp():
-    moving = True
-    while moving:
-        MainCanvas.move(Player, 0, +0.01)
-        sleep(0.5) #CANT HAVE SLEEP REMEMBER? IT BLOCKS TKINTER
 
-def StartPlayerMoveUpThread(event):
-    MoveThread = threading.Thread(target=StartPlayerMoveUp, daemon=True)
-    MoveThread.start()
 
-#def StopPlayerMoveUp():
 
-#def StartPlayerMoveDown():
 
-#def StopPlayerMoveDown():
+HorizontalMoveIncrement = 0
+VerticalMoveIncrement = 0
+
+
+
+def PlayerMovingThread():
+    global root
+    MainCanvas.move(Player, HorizontalMoveIncrement, VerticalMoveIncrement)
+    root.after(30,PlayerMovingThread)
+ 
+
+
+def StartPlayerMoveUp(event):
+    global VerticalMoveIncrement
+    VerticalMoveIncrement = -3    
+    
+def StopPlayerMoveUp(event):
+    global VerticalMoveIncrement
+    VerticalMoveIncrement = 0
+
+
+
+def StartPlayerMoveDown(event):
+    global VerticalMoveIncrement
+    VerticalMoveIncrement = 3
+
+def StopPlayerMoveDown(event):
+    global VerticalMoveIncrement
+    VerticalMoveIncrement = 0
+
+
+
+def StartPlayerMoveLeft(event):
+    global HorizontalMoveIncrement
+    HorizontalMoveIncrement = -3
+
+def StopPlayerMoveLeft(event):
+    global HorizontalMoveIncrement
+    HorizontalMoveIncrement = 0
+
+
+
+def StartPlayerMoveRight(event):
+    global HorizontalMoveIncrement
+    HorizontalMoveIncrement = 3
+
+def StopPlayerMoveRight(event):
+    global HorizontalMoveIncrement
+    HorizontalMoveIncrement = 0
+
+
+
 
 
 root = tk.Tk()
@@ -27,10 +68,21 @@ MainCanvas.pack()
 
 Player = MainCanvas.create_rectangle(450,250,550,350,fill="Blue")
 
-root.bind('<KeyPress-Up>', StartPlayerMoveUpThread)
-#root.bind('<KeyPress-Up>', StopPlayerMoveUpThread)
-#root.bind('<KeyRelease-Down>', StartPlayerMoveDownThread)
-#root.bind('<KeyRelease-Down>', StopPlayerMoveDownThread)
+
+
+root.bind('<KeyPress-Up>', StartPlayerMoveUp)
+root.bind('<KeyRelease-Up>', StopPlayerMoveUp)
+root.bind('<KeyPress-Down>', StartPlayerMoveDown)
+root.bind('<KeyRelease-Down>', StopPlayerMoveDown)
+root.bind('<KeyPress-Left>', StartPlayerMoveLeft)
+root.bind('<KeyRelease-Left>', StopPlayerMoveLeft)
+root.bind('<KeyPress-Right>', StartPlayerMoveRight)
+root.bind('<KeyRelease-Right>', StopPlayerMoveRight)
+
+PlayerMovingThread()
+
+#MoveThread = threading.Thread(target=PlayerMovingThread, daemon=True)
+#MoveThread.start()
 
 root.focus_set()
 root.mainloop()
