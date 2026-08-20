fe7a4c1a5c89408ac3acf867915a05cd6f192fe8
Mat77100 <matteo.porrera@icloud.com>
2026-08-20 10:57:05 +0100

    Add files via upload

--- #Roulette.py
+++ #Roulette.py
@@ -1,21 +1,19 @@
 #Roulette
 import subprocess,random
-
+print("\033[32m")
 Balance = 100
 print(f"Your current balance is ${Balance}, However you can increase it.")
 print("Heres how the game works, there are 5 numbers, one has consquences, the rest will increase the amount. the more numbers you uncover, the higher you increse (i.e: 1 number = 1.4x, 2 numbers = 1.8x etc)")
 
-def GameRound(Balance,MoneyMultiplyer):
+def GameRound(Balance,MoneyMultiplyer,NumbersLeft,BadNumber):
     if len(NumbersLeft) == 1:
         print("Well lucky you.")
         return Balance
-    BadNumber = random.randint(1,5)
-    NumbersLeft = set([1,2,3,4,5])
     MoneyMultiplyer + 0.4
     print(f"Current balance: {Balance}, Next multiplyer: {MoneyMultiplyer}")
-    print(len(NumbersLeft)," Numbers remain")
+    print("\033[31m",len(NumbersLeft)," Numbers remain","\033[32m")
     print("So... pick one of these numbers: ", NumbersLeft)
-
+    SelectedNumber = 6
     while not(SelectedNumber in NumbersLeft):
         try:
             SelectedNumber = int(input())
@@ -27,17 +25,23 @@ def GameRound(Balance,MoneyMultiplyer):
     else:
         print("lucky")
         NumbersLeft.remove(SelectedNumber)
-        Balance * MoneyMultiplyer
-        GameRound(Balance,MoneyMultiplyer)
+        Balance *= MoneyMultiplyer
+        GameRound(Balance,MoneyMultiplyer,NumbersLeft,BadNumber)
 
-Balance = GameRound(Balance)
+
+
+BadNumber = random.randint(1,5)
+NumbersLeft = set([1,2,3,4,5])
+Balance = GameRound(Balance,1,NumbersLeft,BadNumber)
 print(f"Congratulations you made it out with ${Balance}")
 while True:
     print("But you could get even more, if you play again")
     print("Type AGAIN if you want to increase your money, otherwise, type QUIT to quit while you still can. Remember, once your in, you cant leave until youve finished the round")
     Decision = str(input())
     if Decision == "AGAIN":
-        Balance = GameRound(Balance)
+        BadNumber = random.randint(1,5)
+        NumbersLeft = set([1,2,3,4,5])
+        Balance = GameRound(Balance,1,NumbersLeft,BadNumber)
         print(f"Congratulations you made it out with ${Balance}")
     elif Decision == "QUIT":
         break
