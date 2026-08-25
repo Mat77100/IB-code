1892d72f7d312ef11cf931ec74b727f110e83c09
Mat77100 <matteo.porrera@icloud.com>
2026-08-25 14:06:31 +0100

    Update CLEAN algorythm test.py

--- CLEAN algorythm test.py
+++ CLEAN algorythm test.py
@@ -1,5 +1,20 @@
 #25/08/2026
 #CLEAN algorythm test
+import numpy as np
+import matplotlib.pyplot as plt
 
-TrueArray = [0,0,0,0,1,0,0,0,0,0,0,0,0,0]
-DirtyBeam = []
+N = 256
+
+#True sky/1D arr
+TrueArr = np.zeros(N)
+TrueArr[60] = 1.0
+TrueArr[120] = 0.6
+TrueArr[180] = 0.8
+print(TrueArr)
+
+#Sampling function and creating the dirty beam
+Samples = np.zeros(N)
+Samples[N//2] = 1
+for x in [5,10,28,42]:
+    Samples[N//2 + x]
+    Samples[N//2 - x]
