e74fd8b26a582bf2fc277a3c222fe51b43bfa957
Mat77100 <matteo.porrera@icloud.com>
2026-08-25 14:53:00 +0100

    Update CLEAN algorythm test.py

--- CLEAN algorythm test.py
+++ CLEAN algorythm test.py
@@ -10,7 +10,6 @@ TrueArr = np.zeros(N)
 TrueArr[60] = 1.0
 TrueArr[120] = 0.6
 TrueArr[180] = 0.8
-print(TrueArr)
 
 #Sampling function and creating the dirty beam
 Samples = np.zeros(N)
@@ -18,3 +17,19 @@ Samples[N//2] = 1
 for x in [5,10,28,42]:
     Samples[N//2 + x]
     Samples[N//2 - x]
+
+#actually making the dirty beam
+DirtyBeam = np.fft.ifft(Samples).real
+DirtyBeam /= DirtyBeam.max()
+plt.plot(TrueArr)
+plt.show()
+plt.plot(Samples)
+plt.show()
+plt.plot(DirtyBeam)
+plt.show()
+
+#convolve into dirty image
+DirtyArr = np.convolve(TrueArr, DirtyBeam, mode="same")
+plt.plot(DirtyArr)
+plt.show()
+
