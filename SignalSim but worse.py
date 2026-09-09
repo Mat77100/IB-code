671981f3517f52ecd491ea5e395074f43fdd52ab
Mat77100 <matteo.porrera@icloud.com>
2026-09-09 10:56:06 +0100

    Update SignalSim but worse.py

--- SignalSim but worse.py
+++ SignalSim but worse.py
@@ -1,2 +1,8 @@
 #made 09/09/26
 #bored and wanted to ply VotV, but cant cuz chromebook, so ill make smth instead
+import numpy as np
+import sounddevice as sd
+
+Audio = np.random.uniform(1,-1,44100)
+sd.play(Audio,44100)
+sd.wait
